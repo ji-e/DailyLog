fa2851bfdc237261ce08550f32404f217e842487
ji-e <rksk1076@naver.com>
2018-09-27 22:04:41 +0900

    1. 프로젝트 생성 2. 이미지 추가

---
+++ app/src/main/java/com/example/uohih/dailylog/Consts/DLogBaseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.uohih.dailylog.Consts
+
+import android.app.Activity
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DLogBaseActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/uohih/dailylog/main/IntroActivity.kt
@@ -0,0 +1,13 @@
+package com.example.uohih.dailylog.main
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.uohih.dailylog.R
+
+class IntroActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
